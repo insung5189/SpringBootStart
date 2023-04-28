781ef9f04e9cb364716ccdc06ff750b6b095cc83
insung5189 <insung5189@gmail.com>
2023-04-28 13:50:30 +0900

    SPRINGBOOT START!!

--- src/main/java/com/example/sbb/MainController.java
+++ src/main/java/com/example/sbb/MainController.java
@@ -3,12 +3,12 @@ package com.example.sbb;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-
 @Controller
 public class MainController {
+//    @ResponseBody
+    @GetMapping("/sbb") // 클라이언트가 뭘 달라는 요청
     @ResponseBody
-    @GetMapping("/sbb")
     public String index () {
-        return "안녕하소";
+        return "안녕하세요 SBB에 오신것을 환영합니다";
     }
 }
